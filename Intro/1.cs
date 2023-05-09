832c0b1c0c0992502806eecaef7d097db7faf0fe
therealbeez <gabriel.agbese2@gmail.com>
2023-05-09 11:24:13 +0200

    init

---
+++ Intro/1.cs
@@ -0,0 +1,8 @@
+// //Click to download the Prototype 1 Starter Files, then extract the compressed folder.
+// Windows: Right-click on the file > Extract All 
+// Mac: Double-click on the file
+// From the top menu in Unity, select Assets > Import Package > Custom Package, then navigate to the folder you extracted and select the Prototype-1_Starter-Files.unitypackage file.
+// In the Import Unity Package window that pops up, select Import and wait for the assets to import.
+// In the Project window, in Assets > Scenes > double-click on the Prototype 1 scene to open it
+// Delete the Sample Scene without saving
+// Right-click + drag to look around at the start of the road
